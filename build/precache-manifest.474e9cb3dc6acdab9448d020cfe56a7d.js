6e796fd86083bd9c20b55a6deba3ba26605a2499
johnkeik <johnkeik@users.noreply.github.com>
2020-09-18 14:02:45 +0300

    update

---
+++ build/precache-manifest.474e9cb3dc6acdab9448d020cfe56a7d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ab1f561c3b90c17e909714c6f67df9",
+    "url": "/myWebsite/index.html"
+  },
+  {
+    "revision": "a2e80e82edf98b5774af",
+    "url": "/myWebsite/static/css/main.b8680fa1.chunk.css"
+  },
+  {
+    "revision": "fa56440de5db6c4a02a0",
+    "url": "/myWebsite/static/js/2.5eb64a0e.chunk.js"
+  },
+  {
+    "revision": "2927c17f155caba9c734acdc50e6f829",
+    "url": "/myWebsite/static/js/2.5eb64a0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2e80e82edf98b5774af",
+    "url": "/myWebsite/static/js/main.628d0d1b.chunk.js"
+  },
+  {
+    "revision": "dd9d5a4fea17a4a1d696",
+    "url": "/myWebsite/static/js/runtime-main.0910699c.js"
+  },
+  {
+    "revision": "9edc275ebfc8a6ce87e92b5e6c0811b5",
+    "url": "/myWebsite/static/media/CV-Europass-Keikoglou-EN.9edc275e.pdf"
+  },
+  {
+    "revision": "cd3df888019424f57399a3f637a3b5f9",
+    "url": "/myWebsite/static/media/counter_Dart.cd3df888.jpg"
+  },
+  {
+    "revision": "7c36b0a0bee8dd6ad81e02b06e726e74",
+    "url": "/myWebsite/static/media/desktop-trans.7c36b0a0.png"
+  },
+  {
+    "revision": "9541f5353dfc0f2b51b8cccd3fa7108f",
+    "url": "/myWebsite/static/media/tablet-trans.9541f535.png"
+  },
+  {
+    "revision": "5ddfbfc535bad51bff414880062ab338",
+    "url": "/myWebsite/static/media/web-trans.5ddfbfc5.png"
+  }
+]);
